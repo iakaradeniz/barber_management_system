963594c750b040e93873a0547b6c986f242e7981
Karadeniz47 <ismailalperkaradeniz@gmail.com>
2024-12-14 12:11:11 +0300

    Veritabanı_Baglantisi

---
+++ barber_management_system/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace barber_management_system.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
+    }
+}
